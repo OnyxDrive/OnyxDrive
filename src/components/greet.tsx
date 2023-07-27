71fca3b0085abf178e385ba09ceb237ad3466f66
Ilya Klimenko <sn0wy-0wl@yandex.ru>
2023-07-27 16:33:07 +0300

    [OD-2] Moved to app router

---
+++ src/components/greet.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+import { useEffect } from 'react'
+import { invoke } from '@tauri-apps/api/tauri'
+
+export default function Greet() {
+	useEffect(() => {
+		invoke<string>('greet', { name: 'Next.js' })
+			.then(console.log)
+			.catch(console.error)
+	}, [])
+
+	// Necessary because we will have to use Greet as a component later.
+	return <></>
+}
